f0625497cfb73199be5c6a4fbf1668f04523ef7b
Drita Shujaku <dritashujaku@odysseyprime.nl>
2019-10-15 14:44:41 +0200

    configure webpack, babel and css/style loaders

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader'}
+        ]
+      }
+    ]
+  }
+}
